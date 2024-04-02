baf583a656f493fac181475fc36a4b3ebceebeb2
ZachCHale <ZH_Dev@pm.me>
2024-04-02 15:10:21 -0400

    Added custom User Game Settings

---
+++ Source/MyProject/Private/MyGameUserSettings.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameUserSettings.h"
+
+UMyGameUserSettings::UMyGameUserSettings(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	AudioVolumeEffects = 1.0f;
+	AudioVolumeMusic = 1.0f;
+	AudioVolumeMaster = 1.0f;
+}
+
+UMyGameUserSettings* UMyGameUserSettings::GetMyGameUserSettings()
+{
+	return Cast<UMyGameUserSettings>(UGameUserSettings::GetGameUserSettings());
+}
+
+void UMyGameUserSettings::SetAudioVolumeMaster(float NewValue)
+{
+	AudioVolumeMaster = NewValue;
+}
+
+void UMyGameUserSettings::SetAudioVolumeMusic(float NewValue)
+{
+	AudioVolumeMusic = NewValue;
+}
+
+void UMyGameUserSettings::SetAudioVolumeEffects(float NewValue)
+{
+	AudioVolumeEffects = NewValue;
+}
+
+float UMyGameUserSettings::GetAudioVolumeMaster() const
+{
+	return  AudioVolumeMaster;
+}
+
+float UMyGameUserSettings::GetAudioVolumeMusic() const
+{
+	return AudioVolumeMusic;
+}
+
+float UMyGameUserSettings::GetAudioVolumeEffects() const
+{
+	return AudioVolumeEffects;
+}
---
+++ Source/MyProject/Public/MyGameUserSettings.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "MyGameUserSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UMyGameUserSettings : public UGameUserSettings
+{
+	GENERATED_UCLASS_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+	static UMyGameUserSettings* GetMyGameUserSettings();
+	
+	UFUNCTION(BlueprintPure)
+	float GetAudioVolumeMaster() const;
+	UFUNCTION(BlueprintPure)
+	float GetAudioVolumeMusic() const;
+	UFUNCTION(BlueprintPure)
+	float GetAudioVolumeEffects() const;
+	UFUNCTION(BlueprintCallable)
+	void SetAudioVolumeMaster(float NewValue);
+	UFUNCTION(BlueprintCallable)
+	void SetAudioVolumeMusic(float NewValue);
+	UFUNCTION(BlueprintCallable)
+	void SetAudioVolumeEffects(float NewValue);
+
+protected:
+	UPROPERTY(Config)
+	float AudioVolumeMaster;
+	UPROPERTY(Config)
+	float AudioVolumeMusic;
+	UPROPERTY(Config)
+	float AudioVolumeEffects;
+
+};
